a168cb760c7225dbe7b78476c976fe7e82c74d04
ChaseDDevelopment <Chase@ChaseDDevelopment.tech>
2026-04-19 15:55:47 -0600

    Re-add hardtime to nvim pack.add; drop orphan lock entries
    The <leader>uH keymap and whichkey entry referenced hardtime.nvim but it
    was missing from vim.pack.add, so pressing the binding errored. Add the
    plugin back and purge stale oil.nvim + catppuccin/nvim lock entries that
    had no live references.

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -47,6 +47,7 @@ vim.pack.add({
 	"https://github.com/Weyaaron/nvim-training",
 	"https://github.com/coder/claudecode.nvim",
 	"https://github.com/christoomey/vim-tmux-navigator",
+	"https://github.com/m4xshen/hardtime.nvim",
 })
 require("plugins/tokyonight")
 require("plugins/treesitter")
